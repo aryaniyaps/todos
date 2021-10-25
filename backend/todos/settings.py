fc3906972b44229bb39792c2595e0b2e036a80b3
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-25 11:16:13 +0530

    update docker config

--- backend/todos/settings.py
+++ backend/todos/settings.py
@@ -4,7 +4,7 @@ from starlette.config import Config
 config = Config(env_file=".env")
 
 # whether the application is in development mode.
-DEBUG: bool = config("DEBUG", cast=bool, default=True)
+DEBUG: bool = config("FASTAPI_DEBUG", cast=bool, default=True)
 
 # sqlalchemy database url.
 DATABASE_URL: str = config("DATABASE_URL", cast=str)
