8d58c1edd501bdf733dbddcb4a196ddea0a102ec
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-30 21:16:04 +0530

    update template helpers

--- backend/backend/utils/templates.py
+++ backend/backend/utils/templates.py
@@ -8,11 +8,11 @@ environment = Environment(
 )
 
 
-def render_template(template_name: str, *args, **kwargs):
+def render_template(template_name: str, **kwargs):
     """
     Renders the template found with the given variables.
 
     :param template_name: The name of the template to find.
     """
     template = environment.get_template(name=template_name)
-    return template.render(*args, **kwargs)
+    return template.render(**kwargs)
