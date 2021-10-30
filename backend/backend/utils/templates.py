58a63f2aaed03bb8d3ae74d36dc5370108608255
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-30 18:39:25 +0530

    add templateS

---
+++ backend/backend/utils/templates.py
@@ -0,0 +1,18 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+__all__ = ("render_template",)
+
+environment = Environment(
+    loader=PackageLoader("backend"),
+    autoescape=select_autoescape(),
+)
+
+
+def render_template(template_name: str, *args, **kwargs):
+    """
+    Renders the template found with the given variables.
+
+    :param template_name: The name of the template to find.
+    """
+    template = environment.get_template(name=template_name)
+    return template.render(*args, **kwargs)
