3747616879ea3159dd97ce0a7f27daee391f3e75
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-31 18:39:05 +0530

    add faker

--- backend/tests/test_services/test_users.py
+++ backend/tests/test_services/test_users.py
@@ -1,36 +1,36 @@
 import pytest
+from faker import Faker
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.services.users import create_user, deactivate_user
 
 
 @pytest.mark.asyncio
-async def test_create_user(session: AsyncSession) -> None:
+async def test_create_user(session: AsyncSession, faker: Faker) -> None:
     """
     Ensure we can create an user.
     """
+    email = faker.ascii_safe_email()
+    password = faker.password(length=12)
     user = await create_user(
         session=session,
-        email="alpha@abc.com",
-        username="alpha",
-        password="password"
+        email=email,
+        password=password
     )
     assert user.is_active
-    assert user.check_password("password")
-    assert user.username == "alpha"
-    assert user.email == "alpha@abc.com"
+    assert user.check_password(password)
+    assert user.email == email
 
 
 @pytest.mark.asyncio
-async def test_deactivate_user(session: AsyncSession) -> None:
+async def test_deactivate_user(session: AsyncSession, faker: Faker) -> None:
     """
     Ensure we can deactivate an user.
     """
     user = await create_user(
         session=session,
-        email="alpha@abc.com",
-        username="alpha",
-        password="password"
+        email=faker.ascii_safe_email(),
+        password=faker.password(length=12)
     )
     await deactivate_user(session=session, user=user)
     assert not user.is_active
