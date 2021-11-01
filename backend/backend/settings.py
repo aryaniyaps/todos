543538c7336e2b8e6b30312ab022253bd33f3151
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-01 09:56:01 +0530

    add more settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from starlette.config import Config
+from starlette.datastructures import Secret
 
 
 config = Config()
@@ -11,3 +14,18 @@ DATABASE_URL: str = config("DATABASE_URL", cast=str)
 
 # elasticsearch configuration.
 ELASTICSEARCH_URL: str = config("ELASTICSEARCH_URL", cast=str)
+
+# mail client host name.
+MAIL_HOST: str = config("MAIL_HOST", cast=str)
+
+# mail client port.
+MAIL_PORT: int = config("MAIL_PORT", cast=int)
+
+# mail client auth username.
+MAIL_USERNAME: Optional[str] = config("MAIL_USERNAME", cast=str, default=None)
+
+# mail client auth password.
+MAIL_PASSWORD: Optional[Secret] = config("MAIL_PASSWORD", cast=Secret, default=None)
+
+# mail client sender address.
+MAIL_SENDER: Optional[str] = config("MAIL_SENDER", cast=str, default=None)
