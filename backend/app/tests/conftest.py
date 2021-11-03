c79720f3c426ae3474f08fb0457fea74134d7d7d
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-03 19:08:06 +0530

    update conftest.py

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
 from app.extensions import db
 
 
-@pytest.fixture(scope="function")
-def app():
+@pytest.fixture(scope="session")
+def app() -> Flask:
     app = create_app()
     context = app.test_request_context()
     context.push()
@@ -13,14 +16,20 @@ def app():
     context.pop()
 
 
+@pytest.fixture(scope="function")
+def client(app: Flask) -> FlaskClient:
+    yield app.test_client()
+
+
 @pytest.fixture(scope="session")
-def db(app):
+def db(app: Flask) -> SQLAlchemy:
     db.create_all()
     yield db
     db.drop_all()
 
 
 @pytest.fixture(scope="function")
-def session(db):
+def session(db: SQLAlchemy):
+    db.session.begin_nested()
     yield db.session
-    db.session.remove()
+    db.session.rollback()
