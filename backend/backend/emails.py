d20a871642ce460088d5695038d05c429ddc35bf
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-03 08:00:24 +0530

    add email helper

---
+++ backend/backend/emails.py
@@ -0,0 +1,17 @@
+from backend.extensions import mail
+from flask_mail import Message
+
+
+def send_mail(to: str, subject: str, html: str, body: str) -> None:
+    """
+    Sends an individual mail to the
+    specified recipient, with the given
+    subject and template.
+    """
+    message = Message(
+        subject=subject,
+        body=body,
+        html=html
+    )
+    message.add_recipient(to)
+    mail.send(message)
