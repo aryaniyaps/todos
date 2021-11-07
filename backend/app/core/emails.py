c574e37c48deb746f93ece5fc501980f30d1261d
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-07 19:09:20 +0530

    update emails.py

--- backend/app/core/emails.py
+++ backend/app/core/emails.py
@@ -1,23 +1,19 @@
-from typing import Optional
-
+from flask.templating import render_template
 from flask_mail import Message
 
 from app.extensions import mail
+from app.models.users import User
 
 
-def send_mail(to: str, subject: str, body: str, html: Optional[str] = None) -> None:
+def send_user_created_mail(recipient: str, user: User) -> None:
     """
-    Sends an individual email.
+    Sends an mail to a newly created user.
 
-    :param to: The recipient of the email.
-    :param subject: The subject of the email.
-    :param html: The HTML content of the email.
-    :param body: The content of the email.
+    :param recipient: The address to send the mail to.
+    :param user: The newly created user.
     """
-    message = Message(
-        subject=subject, 
-        body=body, 
-        html=html
-    )
-    message.add_recipient(to)
-    mail.send(message)
+    subject = f"New account created"
+    html = render_template("emails/user_created.html", user=user)
+    message = Message(subject=subject, html=html)
+    message.add_recipient(recipient=recipient)
+    mail.send(message=message)
