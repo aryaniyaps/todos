9c9e5ea04a7ee50e03b864242d94590df380bbaa
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-14 19:35:18 +0530

    add axios

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -5,8 +5,8 @@ export const BaseLayout: React.FC = (props) => {
     return (
         <div className="flex flex-col h-full w-full bg-primary-800">
             <NavBar />
-            <div className="overflow-y-auto">
-                <div className="p-4 flex-grow mx-auto w-full flex max-w-7xl">
+            <div className="overflow-y-auto flex-grow flex">
+                <div className="p-4 flex-grow mx-auto flex max-w-7xl">
                     {props.children}
                 </div>
             </div>
