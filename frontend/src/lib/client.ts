bdeaadd094f5da18acf263957aeca83509184ddb
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-17 10:57:06 +0530

    get webpack working

--- frontend/src/lib/client.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const client = axios.create({
-    baseURL: "/api",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,58 +1,69 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    entry: "./src/index",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    stats: "minimal",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"],
-    },
-    devServer: {
-        client: {
-            overlay: true,
-            progress: true,
-        },
-        static: {
-            directory: path.resolve(__dirname, "public"),
-        },
-        compress: true,
-        port: 3000,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, "public/index.html"),
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: [/\.test.tsx?$/],
-                user: ["babel-loader", "ts-loader"],
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: [/node_modules/, /\.test.jsx?$/],
-                use: ["babel-loader"],
-            },
+// path configuration
+const staticPath = path.resolve(__dirname, "public");
+const appHTML = path.resolve(__dirname, "public/index.html");
+const outputPath = path.resolve(__dirname, "dist");
 
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ["style-loader", "css-loader"],
+module.exports = function (env) {
+    const __prod__ = env == "production";
+
+    return {
+        mode: __prod__ ? "production" : "development",
+        entry: "./src/index",
+        output: {
+            path: outputPath,
+            filename: "bundle.js",
+        },
+        stats: "minimal",
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        devServer: {
+            client: {
+                overlay: true,
+                progress: true,
             },
-            {
-                test: /\.svg$/,
-                use: ["svg-inline-loader"],
+            static: {
+                directory: staticPath,
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
+            compress: true,
+            port: 3000,
+        },
+        plugins: [
+            new HTMLWebpackPlugin({
+                inject: true,
+                template: appHTML,
+            }),
         ],
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: [/\.test.tsx?$/],
+                    use: ["babel-loader", "ts-loader"],
+                },
+                {
+                    test: /\.jsx?$/,
+                    exclude: [/node_modules/, /\.test.jsx?$/],
+                    use: ["babel-loader"],
+                },
+
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.svg$/,
+                    use: ["svg-inline-loader"],
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: "asset/resource",
+                },
+            ],
+        },
+    };
 };
