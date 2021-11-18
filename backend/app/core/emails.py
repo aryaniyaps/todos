6c7133c6d680ac5a045a8ae792ac9ae4fe53cb1a
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-18 07:39:26 +0530

    update backend-tests.yml

--- backend/app/core/emails.py
+++ backend/app/core/emails.py
@@ -12,7 +12,7 @@ def send_user_created_mail(recipient: str, user: User) -> None:
     :param recipient: The address to send the mail to.
     :param user: The newly created user.
     """
-    subject = f"New account created"
+    subject = f"New user created"
     html = render_template("emails/user_created.html", user=user)
     message = Message(subject=subject, html=html)
     message.add_recipient(recipient=recipient)
