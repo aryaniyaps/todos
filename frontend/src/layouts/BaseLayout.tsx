9e5832442679d1ac8c4f292360b7133ab5d9d0ac
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-21 17:38:16 +0530

    update layouts

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export const BaseLayout: React.FC = (props) => {
-    return (
-        <div className="flex flex-col h-full w-full bg-primary-700">
-            {props.children}
-        </div>
-    );
+    return <div className="flex flex-col h-full w-full">{props.children}</div>;
 };
