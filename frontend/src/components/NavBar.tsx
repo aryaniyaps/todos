d2d1e8f9ca433b344e0bd45ef6b78352084fe9f4
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-21 17:40:37 +0530

    update navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const NavBar: FC = () => {
     return (
-        <div className="bg-primary-800 w-full">
+        <div className="bg-primary-300 shadow-sm w-full">
             <div className="max-w-7xl mx-auto p-4 font-semibold">navbar</div>
         </div>
     );
