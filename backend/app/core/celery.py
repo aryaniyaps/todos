bba35b401f218dca48ce2a853dcde0f7b40b6d56
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-22 13:05:14 +0530

    update docker config

--- backend/app/core/celery.py
+++ backend/app/core/celery.py
@@ -1,3 +1,19 @@
-from celery import Celery
+from celery import Celery, Task
+from flask import Flask
 
-celery = Celery(__name__)
+from app import application
+
+
+def create_celery(app: Flask) -> Celery:
+    class ContextTask(Task):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return super().__call__(*args, **kwargs)
+
+    celery = Celery(app.import_name, task_cls=ContextTask)
+    celery.conf.update(app.config)
+    return celery
+
+celery = create_celery(application)
