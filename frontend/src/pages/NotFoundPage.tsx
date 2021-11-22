f4b3c65d5a90bbc6731d7522d437ab434ac3e886
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-22 09:00:48 +0530

    add title to page: not found

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
+import { Helmet } from "react-helmet";
 import { Button } from "../components/Button";
 import { BaseLayout } from "../layouts/BaseLayout";
 
 export const NotFoundPage: FC = () => {
     return (
         <BaseLayout>
+            <Helmet>
+                <title>Not found | Todos</title>
+            </Helmet>
             <div className="flex flex-col text-center flex-grow justify-center items-center p-2">
                 <h1>Page not found</h1>
                 <p>Sorry, we couldn't find the page you were looking for.</p>
