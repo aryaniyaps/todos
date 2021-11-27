cc565d4dab5bb2edfabb5b63c1217bbe3aeeea7e
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-11-27 13:46:38 +0530

    update jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
+    verbose: true,
     testEnvironment: "jsdom",
     testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|svg|woff|woff2)$": "<rootDir>/src/mocks/fileMock.ts",
         "\\.css$": "<rootDir>/src/mocks/styleMock.ts",
     },
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-    verbose: true,
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
