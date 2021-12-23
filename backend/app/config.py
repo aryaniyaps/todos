42ea497a43c19e49256fae2578c91b5ff70045b6
Aryan Iyappan <aryaniyappan2006@gmail.com>
2021-12-23 16:43:03 +0530

    delete dead tasks

--- backend/app/config.py
+++ backend/app/config.py
@@ -15,21 +15,6 @@ SITE_NAME = config("SITE_NAME", cast=str, default="todos")
 # SQLAlchemy database URL.
 DATABASE_URL = config("DATABASE_URL", cast=str)
 
-# mail server name.
-MAIL_SERVER = config("MAIL_SERVER", cast=str, default="localhost")
-
-# mail client port.
-MAIL_PORT = config("MAIL_PORT", cast=int, default=25)
-
-# whether the mail client should use tls.
-MAIL_USE_TLS = config("MAIL_USE_TLS", cast=bool, default=True)
-
-# mail client auth username.
-MAIL_USERNAME = config("MAIL_USERNAME", cast=str)
-
-# mail client auth password.
-MAIL_PASSWORD = config("MAIL_PASSWORD", cast=str)
-
 # Celery broker URL.
 CELERY_BROKER_URL = config("CELERY_BROKER_URL", cast=str)
 
