e6101d707b9ada33ab320f1339a325e5681aef24
Aryan Iyappan <aryaniyappan2006@gmail.com>
2022-01-08 12:50:49 +0530

    update providers

--- backend/app/api/providers.py
+++ backend/app/api/providers.py
@@ -13,6 +13,12 @@ from backend.app.services.todos import TodoService
 
 
 def get_session() -> Generator[Session, None, None]:
+    """
+    Gets a database session, and 
+    closes it when it's returned back.
+
+    :return: The datbase session.
+    """
     session = session_factory()
     try:
         yield session
@@ -26,16 +32,34 @@ def get_session() -> Generator[Session, None, None]:
 def get_service(
     service: Type[BaseService],
 ) -> Callable[[Session], BaseService]:
-    def service_initializer(
+    """
+    Creates the given service.
+
+    :param service: The service to create.
+
+    :return: The created service.
+    """
+    def create_service(
         session: Session = Depends(
             dependency=get_session,
         ),
     ) -> BaseService:
+        """
+        Initializes the given service with
+        a database session.
+
+        :return: The initialized service.
+        """
         return service(session=session)
-    return service_initializer
+    return create_service
 
 
 def get_current_user() -> User:
+    """
+    Gets the current user from the given request.
+
+    :return: The current user.
+    """
     pass
 
 
@@ -50,6 +74,12 @@ def get_todo(
         ),
     ),
 ) -> Todo:
+    """
+    Gets the associated todo from
+    the route's path parameter.
+
+    :return: The fetched todo.
+    """
     todo = todo_service.get_todo(
         todo_id=todo_id, 
         user_id=current_user.id,
