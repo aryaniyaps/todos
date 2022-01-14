3dc2c106a1745a274f95a41105c367c81693fdc0
Aryan Iyappan <aryaniyappan2006@gmail.com>
2022-01-14 19:38:15 +0530

    add gevent

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -5,3 +5,15 @@ bind = "0.0.0.0:8000"
 
 # worker processes
 workers = (cpu_count() * 2) + 1
+
+# worker class
+worker_class = "gevent"
+
+# max requests to be processed
+max_requests = pow(10, 3)
+
+# worker timeout
+timeout = 15
+
+# request waiting time
+keepalive = 5
