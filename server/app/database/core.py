f1257e15a4dcd6cbba3111bd96c745da3260a51e
Aryan Iyappan <69184573+aryaniyaps@users.noreply.github.com>
2022-02-04 17:44:55 +0530

    Update naming convention

--- server/app/database/core.py
+++ server/app/database/core.py
@@ -9,10 +9,10 @@ from app.config import DEBUG, DATABASE_URL
 
 metadata = MetaData(
     naming_convention={
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
+        "ix": "index_%(column_0_label)s",
+        "uq": "unique_%(table_name)s_%(column_0_name)s",
+        "fk": "foreign_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "primary_%(table_name)s",
     },
 )
 
